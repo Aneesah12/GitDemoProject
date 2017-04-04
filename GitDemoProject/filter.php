9f29824fc88478dc7b39e901bc26773cc197ec9a
Aneesah12 <aneesah.quraishi@gmail.com>
2017-04-04 14:07:53 +0100

    Added a filter function/method for checking emails.
    Making sure that the email address users submit are valid (use the
    standard email structure).

--- GitDemoProject/filter.php
+++ GitDemoProject/filter.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require("includes/PHPMailer/class.phpmailer.php");
+
+$mail = new PHPMailer;
+
+if (!$mail->ValidateAddress($email)) {
+    echo "Invalid Email Address";
+    exit;
+}
+
+
+
+
 
